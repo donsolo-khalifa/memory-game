cd6e1d24e4895d8df46d4b6618dac85aca9c17d8
donsolo-khalifa <donsolokhalifa20@gmail.com>
2022-09-07 03:37:50 +0000

    optimized code

--- game.php
+++ game.php
@@ -19,43 +19,36 @@ session_start();
     <h2>
       <?php
 
-      $serverName = "localhost";
-      $dbusername = "root";
-      $dbPassword = "";
-      $dbName = "scoresdatabase";
+      require_once "dbhandler.php";
+
       $userName = $_SESSION["useruId"];
-      $conn = mysqli_connect($serverName, $dbusername, $dbPassword, $dbName);
       $best;
       $rank;
       echo "welcome " . $_SESSION["useruId"];
 
-      if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
 
 
-      $rankQuery = "SELECT scores,usersUid FROM users ORDER BY scores ASC";
+      $rankQuery = "SELECT scores,usersUid FROM users WHERE NOT (scores=0) ORDER BY scores ASC";
 
       $sql = "SELECT scores FROM users WHERE usersUid='$userName'";
 
       $data = array();
 
-      $rankResult = mysqli_query($conn, $rankQuery);
+      $rankResult = mysqli_query($con, $rankQuery);
 
-      $result = mysqli_query($conn, $sql);
+      $result = mysqli_query($con, $sql);
       while ($row = mysqli_fetch_assoc($result)) {
         $best = $row["scores"];
       }
       while ($row = mysqli_fetch_assoc($rankResult)) {
         $data[$row['usersUid']] = $row['scores'];
       }
-      if ($best!=0) {
-        $rank=array_search($userName,array_keys($data))+1;
-
-      }else{
-        $rank="Unranked";
+      if ($best != 0) {
+        $rank = array_search($userName, array_keys($data)) + 1;
+      } else {
+        $rank = "Unranked";
       }
-      mysqli_close($conn);
+      mysqli_close($con);
 
 
 
@@ -87,30 +80,8 @@ session_start();
       <label for="uploadscore">
         <input id="uploadscore" name="uploadscore" readonly hidden>
       </label>
-      <!-- <input  name="submit" type="submit" readonly hidden>
-    -->
-      <?php
-
-      // $serverName="localhost";
-      // $dbusername="root";
-      // $dbPassword="";
-      // $dbName="scoresdatabase";
-      // $userName=$_SESSION["useruId"];
-      // $conn= mysqli_connect($serverName,$dbusername,$dbPassword,$dbName);
-      //       // Create connection
-      //       // Check connection
-      //       if (!$conn) {
-      //         die("Connection failed: " . mysqli_connect_error());
-      //       }
-
-      //       $sql = "SELECT scores FROM users WHERE usersUid='$userName'";
-      //       $result = mysqli_query($conn, $sql);
-      //       while($row = mysqli_fetch_assoc($result)) {
-      //        $_POST["best"]=$row["scores"];
-      //       }
-
-      //       mysqli_close($conn);
-      ?>
+    
+     
 
     </form>
 
--- gameplease.php
+++ gameplease.php
@@ -1,40 +1,68 @@
 <?php
 session_start();
-$serverName="localhost";
-$dbusername="root";
-$dbPassword="";
-$dbName="scoresdatabase";
-$scores=$_POST["uploadscore"];
-$userName=$_SESSION["useruId"];
-$con= mysqli_connect($serverName,$dbusername,$dbPassword,$dbName);
+if (isset($_POST["uploadscore"])) {
+  $scores = $_POST["uploadscore"];
+}
+$userName = $_SESSION["useruId"];
+require_once "dbhandler.php";
 
-$sqli="SELECT scores FROM users WHERE  usersUid='$userName'";
+
+$sqli = "SELECT scores FROM users WHERE  usersUid='$userName'";
+$rankQuery = "SELECT scores,usersUid FROM users WHERE NOT (scores=0) ORDER BY scores ASC";
 $result = mysqli_query($con, $sqli);
+$rankResult = mysqli_query($con, $rankQuery);
+echo '<a href="game.php">Go back to game</a>'.'<br>';
 
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    if ($row["scores"]>$scores||$row["scores"]==0) {
-        updatedb($con,$scores,$userName);
-    }
-    else{
-        echo "You did not pass your high score";
+// output data of each row
+while ($row = mysqli_fetch_assoc($result)) {
+  if (isset($scores)) {
+    if ( $row["scores"] > $scores || $row["scores"] == 0) {
+    
+      updatedb($con, $scores, $userName);
+    } else {
+      echo "You did not pass your high score";
     }
   }
+ 
+}
+echo '<h2> leaderbords</h2>'."<br>";
 
+$position=0;
 
-function updatedb($con,$scores,$userName){
+while ($row = mysqli_fetch_assoc($rankResult)) {
+  $data=array();
 
-    if (!$con) {
-        die("Connection failed".mysqli_connect_error());
+  // echo $row['usersUid']." ". $row['scores'] .'<br>';
+  $data[$row['usersUid']] = $row['scores'];
+  foreach ($data as $key => $value) {
+    $position++;
+    if ($userName ==$key ) {
+
+      echo  '<h1 style="font-style: italic; color:cyan;">' .$position."\t". $key . "\t" . $value . '</h1>' . '<br>';
+    } else{
+      echo $position . "\t" . $key . "\t" . $value . '<br>';
+
     }
     
-    $sql = "UPDATE users SET scores='$scores' WHERE usersUid='$userName'";
-    
-    if (mysqli_query($con, $sql)) {
-      echo "Record updated successfully";
-    } else {
-      echo "Error updating record: " . mysqli_error($con);
-    }
-    
-    mysqli_close($con);
+  }
+}
+
+
+
+function updatedb($con, $scores, $userName)
+{
+
+  if (!$con) {
+    die("Connection failed" . mysqli_connect_error());
+  }
+
+  $sql = "UPDATE users SET scores='$scores' WHERE usersUid='$userName'";
+
+  if (mysqli_query($con, $sql)) {
+    echo "Record updated successfully" . '<br>';
+  } else {
+    echo "Error updating record: " . mysqli_error($con);
+  }
+
+  mysqli_close($con);
 }
